60a0e2a2a35de795d0af84ebedf7e3de07997806
Matthew Slusser <slusser12@gmail.com>
2023-01-26 09:09:05 -0500

    get digital reading from water level sensor

---
+++ src/water_level_sensor.ino
@@ -0,0 +1,23 @@
+/***************************************************
+* Liquid Level Sensor-XKC-Y25-T12V
+* ****************************************************
+* This example is to get liquid level
+
+* @author Matthew Slusser
+* @version  V1.0
+* @date  2023-01-19
+
+* ****************************************************/
+int liquid_level=0;
+int liquid_level_pin=5;
+void setup() {
+ Serial.begin(9600);
+ pinMode(liquid_level_pin,INPUT_PULLUP);
+}
+
+void loop() {
+Liquid_level=digitalRead(liquid_level_pin);
+Serial.print("Liquid_level= ");
+Serial.println(Liquid_level,DEC);
+delay(500);
+}
